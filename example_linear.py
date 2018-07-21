47ae795995b905390a26dc93dd5b1e7f14c81598
YOYOZHAO <37534176+Y-H-Zhao@users.noreply.github.com>
2018-07-21 19:48:06 +0800

    Update example_linear.py

--- example_linear.py
+++ example_linear.py
@@ -5,7 +5,7 @@ inputY = np.multiply(3,inputX)  + 1
 x = tf.placeholder("float32")
 weight = tf.Variable(0.25)
 bias = tf.Variable(0.25)
-y = tf.mul(weight,x) + bias
+y = tf.multiply(weight,x) + bias
 y_ = tf.placeholder("float32")
 loss = tf.reduce_sum(tf.pow((y - y_),2))
 train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
@@ -16,3 +16,5 @@ for _ in range(1000):
     sess.run(train_step,feed_dict={x:inputX,y_:inputY})
     if _%20 == 0:
         print("The value of W is:: ",weight.eval(session=sess),"The value of bias is: " ,bias.eval(session=sess))
+sess.run(weight)
+sess.run(bias)
